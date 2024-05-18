d81cbed687928a1f7a609599ca8cadf552c622f9
SANKETPise95 <sanketbalupise@gmail.com>
2024-05-18 18:50:33 +0530

    18-05-024 commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
 
     // navList Data
     const navList = (
-        <ul className="flex space-x-3 text-white font-medium text-md px-5">
+        <ul className="flex space-x-3 text-green-600 font-medium text-md px-5">
             {/* Home */}
             <li>
                 <Link to={'/'}>Home</Link>
@@ -66,13 +66,14 @@ const Navbar = () => {
         </ul>
     )
     return (
-        <nav className="bg-green-600 sticky top-0 rounded-" >
+        <nav className="bg-white rounded-" >
             {/* main  */}
             <div className="lg:flex lg:justify-between items-center py-3 lg:px-3 ">
                 {/* left  */}
                 <div className="left py-3 lg:py-0">
                     <Link to={'/'}>
-                        <h2 className=" font-bold text-white text-2xl text-center">GardenGrown</h2>
+                   <img src="https://media.tenor.com/k-wL_qZAELgAAAAi/test.gif" class="h-8" alt="Flowbite Logo" />  <h2 className=" font-bold text-green-600 text-2xl text-center">GardenGrown</h2>
+
                     </Link>
                 </div>
 
